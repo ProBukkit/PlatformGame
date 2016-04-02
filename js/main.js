d2ef047c44c17ce448c85aeec6b2b1ecb3197f68
J B <J B>
2016-04-02 03:13:59 -0400

    ball

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ var dy = -2;
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
 }
